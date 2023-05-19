c3884c353a4930abadb5cf54e1f860204c3db605
Ethan Hutchins <ethanhutchins33@gmail.com>
2023-05-19 16:16:00 +0100

    Initial Commit

---
+++ BlobberConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Storage.Blobs;
+
+const string blobContainerName = "blobbercontainer";
+
+string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+
+var blobServiceClient = new BlobServiceClient(connectionString);
+
+var blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
+
+// Create a local file in the ./data/ directory for uploading and downloading
+string localPath = "data";
+Directory.CreateDirectory(localPath);
+string fileName = "quickstart" + Guid.NewGuid().ToString() + ".txt";
+string localFilePath = Path.Combine(localPath, fileName);
+
+// Write text to the file
+await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+// Get a reference to a blob
+BlobClient blobClient = blobContainerClient.GetBlobClient(fileName);
+
+Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+// Upload data from the local file
+await blobClient.UploadAsync(localFilePath, false);
+
